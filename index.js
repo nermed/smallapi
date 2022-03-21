3e5b7c023cfc110710e7b7fe6f19cdba1a4e167a
nermed <donnermed@gmail.com>
2022-03-21 11:25:10 +0200

    add access-control-allow-origin le 21/03 11:25

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const axios = require('axios')
 const bodyParser = require('body-parser')
 
 app.use(
-  cors(),
+  cors({
+    credentials: true,
+    origin: true,
+  }),
 )
-// app.options('*', cors())
+app.options('*', cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -14,8 +17,8 @@ app.get('/', (req, res) => res.send('Cool!!!'))
 
 app.post('/getInvoice', async function (req, res) {
   let token = ''
-  let login = req.body.loginData
-  let signature = req.body.signature
+  let login = req.body.loginData;
+  let signature = req.body.signature;
   // console.log(req.body.signature);return;
   await connect(login).then((data) => (token = data))
   if (token) {
@@ -26,7 +29,6 @@ app.post('/getInvoice', async function (req, res) {
           { invoice_signature: signature },
           {
             headers: {
-              'Access-Control-Allow-Origin': '*',
               'Content-Type': 'application/x-www-form-urlencoded',
               Authorization: 'Bearer ' + token,
             },
@@ -56,7 +58,6 @@ app.post('/check', async function (req, res) {
           { tp_TIN: nif },
           {
             headers: {
-              'Access-Control-Allow-Origin': '*',
               'Content-Type': 'application/x-www-form-urlencoded',
               Authorization: 'Bearer ' + token,
             },
@@ -78,13 +79,12 @@ app.post('/check', async function (req, res) {
 
 app.post('/request', async function (req, res) {
   var loginData = req.body.loginData
-  var data = req.body.data
+  var data = req.body.data;
   var token = ''
-  var error = false
+  var error = false;
   // console.log(loginData);return;
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Access-Control-Allow-Origin': '*',
   }
   const ress = await axios
     .post('http://41.79.226.28:8345/ebms_api/login', loginData, { headers })
@@ -94,31 +94,28 @@ app.post('/request', async function (req, res) {
   if (token.length > 0) {
     try {
       const dataa = await axios
-        .post('http://41.79.226.28:8345/ebms_api/addInvoice', data, {
+      .post(
+        'http://41.79.226.28:8345/ebms_api/addInvoice',
+        data,
+        {
           headers: {
-            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/x-www-form-urlencoded',
             Authorization: 'Bearer ' + token,
           },
-        })
-        .then((rep) => {
-          console.log('rep -> ', rep)
-          res.status(rep.status).send(rep.data)
-          return
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response.status)
-            res
-              .status(error.response.status)
-              .send(
-                `${error.response.data.msg} (${
-                  JSON.parse(data).invoice_number
-                })`,
-              )
-            return
-          }
-        })
+        },
+      )
+      .then((rep) => {
+        console.log('rep -> ', rep);
+        res.status(rep.status).send(rep.data);
+        return;
+      })
+      .catch((error) => {
+        if(error.response) {
+          console.log(error.response.status);
+          res.status(error.response.status).send(`${error.response.data.msg} (${JSON.parse(data).invoice_number})`)
+          return;
+        }
+      })
     } catch (e) {
       res.status(e).send('erreur' + e)
     }
@@ -131,48 +128,46 @@ app.listen(process.env.PORT || port, () => {
 })
 
 const requesting = async (datas, token, verify, t = 0) => {
-  let errorRender = null
-  let status = null
+  let errorRender = null;
+  let status = null;
   const dataa = await axios
     .post(
       'http://41.79.226.28:8345/ebms_api/addInvoice',
       JSON.stringify(datas[t]),
       {
         headers: {
-          'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/x-www-form-urlencoded',
           Authorization: 'Bearer ' + token,
         },
       },
     )
     .then((rep) => {
-      verify = true
+      verify = true;
     })
     .catch((error) => {
-      errorRender = `${error.response.data.msg} (${datas[t].invoice_number})`
+      errorRender = `${error.response.data.msg} (${datas[t].invoice_number})`;
       status = error.response.status
-      verify = false
+      verify = false;
     })
-  if (verify) {
-    let tt = t + 1
-    let end = false
-    console.log('ok ', datas[t].invoice_number)
-    if (tt < datas.length) {
-      requesting(datas, token, verify, tt)
-      console.log('suivant -> ', datas[tt].invoice_number)
+    if(verify) {
+      let tt = t + 1;
+      let end = false;
+      console.log('ok ', datas[t].invoice_number);
+      if(tt < datas.length) {
+        requesting(datas, token, verify, tt);
+        console.log('suivant -> ', datas[tt].invoice_number);
+      } else {
+        console.log('finis');
+        return {errorRender:'Finis', verify: false, status: 201};
+      }
     } else {
-      console.log('finis')
-      return { errorRender: 'Finis', verify: false, status: 201 }
+      return {errorRender, verify, status};
     }
-  } else {
-    return { errorRender, verify, status }
-  }
 }
 async function connect(loginData) {
   let token = null
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/x-www-form-urlencoded',
   }
   const ress = await axios
